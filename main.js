5cf0cab78edbf8e4e2abe66943962b35581298d4
Grahanam <lunasuthar5221@gmail.com>
2024-03-03 14:51:21 +0530

    carousel responsive width

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 // mobile carousel intialization function
 jQuery(document).ready(function(){
-  $(".owl-carousel").owlCarousel();
+  $(".owl-carousel").owlCarousel({
+    loop:true,
+    nav:true,
+    responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:1
+      },
+      1000:{
+          items:1
+      }
+  }
+  })
 });
 
 
